1766384e8524f18f46cbec78bcf12029121f37f6
Bast1onic <al6253@nyu.edu>
2025-04-05 15:51:42 -0400

    Search homepage

--- server.mjs
+++ server.mjs
@@ -1,21 +1,43 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { create } from 'express-handlebars';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 // Load environment variables from .env file
 dotenv.config();
 
+// Convert the module URL to a file path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 const PORT = process.env.PORT || 3000; // Use PORT from .env or default to 3000
 
+// Set up Handlebars
+const hbs = create({
+    extname: '.hbs',
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials'),
+});
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+
 // Middleware
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Welcome to your starter Express app!');
-});
+    res.render('home', { siteTitle: 'YouSearch' }); // Pass dynamic data
+  });
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+	console.log(`Server is running on http://localhost:${PORT}`);
 });
